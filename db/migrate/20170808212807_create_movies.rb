24703944e0464e19722f5edeff4db995249e7ef3
DanL12186 <danlrcn@hotmail.com>
2017-08-08 18:07:01 -0400

    added movie model

---
+++ db/migrate/20170808212807_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :year
+      t.integer :rating
+      t.integer :votes
+      t.string :mpaa
+
+      t.timestamps
+    end
+  end
+end
